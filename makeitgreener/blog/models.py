ab5bdbe183ac0fc76ffe91ae68b7052c200b6f8f
lyf2000 <lyf2000@mail.ru>
2020-04-17 11:25:32 +0300

    add Post model n admin

--- makeitgreener/blog/models.py
+++ makeitgreener/blog/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
+
+    title = models.CharField(max_length=125)
