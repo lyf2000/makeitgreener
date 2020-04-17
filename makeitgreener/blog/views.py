75da42e276ee926ae33ca0d906ec30171749334c
lyf2000 <lyf2000@mail.ru>
2020-04-17 16:29:50 +0300

    silve conflicts

--- makeitgreener/blog/views.py
+++ makeitgreener/blog/views.py
@@ -9,11 +9,11 @@ def index(request):
         print('AJAX')
         coords = ast.literal_eval(request.POST.get('coords', None))
         return JsonResponse({'data': 'OK'})
-<<<<<<< HEAD
+
     return render(request, 'blog/index.html')
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
-=======
+
     return render(request, 'index.html')
->>>>>>> develop
+
