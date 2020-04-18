4fc01dff2a0aa8276babbba06e8260c3b0bbda47
lyf2000 <lyf2000@mail.ru>
2020-04-18 14:44:28 +0300

    tags in Post n admin

--- makeitgreener/blog/admin.py
+++ makeitgreener/blog/admin.py
@@ -1,9 +1,33 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
+from taggit.forms import TagField
+from taggit_labels.widgets import LabelWidget
+
 from blog.models import Post
+
+
+class PostForm(forms.ModelForm):
+    tags = TagField(required=False, widget=LabelWidget)
+
+    class Meta:
+        model = Post
+        exclude = []
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    form = PostForm
+
+    fieldsets = (
+        ('Standard info', {
+            'fields': ('title', 'author'
+                       ),
+        }),
+        ('Tags', {
+            'fields': ('tags',
+                       ),
+        })
+    )
+
