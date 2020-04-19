4b32f4c4fc796f5126357d4bf82b0cc5b22165f2
lyf2000 <lyf2000@mail.ru>
2020-04-19 15:01:12 +0300

    adding tags from input and rendering

--- makeitgreener/templates/test/my.js
+++ makeitgreener/templates/test/my.js
@@ -1,5 +1,24 @@
 $(document).keyup(function (e) {
     if ($("#nin:focus") && (e.keyCode === 13)) {
-       alert('ya!')
+
     }
- });
+});
+
+$(document).keyup(function (e) {
+
+    if ($("#select-tag-input:focus") && (e.keyCode === 13)) {
+        let selectTagInput = $("#select-tag-input");
+        let newTagName = selectTagInput.val().replace(/ /g,'').toLowerCase();
+        if (newTagName !== '') {
+            addNewSelectedTagName(newTagName);
+            selectTagInput.val('');
+        }
+    //    TODO error messge - red
+    }
+});
+
+let addNewSelectedTagName = (tagName) => {
+    $('#selected-tags-list').append(`<h6 class="inline"><span class="badge badge-primary"><a href="#"
+                                                                    style="color: white;text-decoration:none">&times;</a> ${tagName}</span>
+            </h6>`)
+};
