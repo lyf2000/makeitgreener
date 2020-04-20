7710e5e9c0e9cf05e21dc21ed561452da65a8bf6
lyf2000 <lyf2000@mail.ru>
2020-04-20 10:12:13 +0300

    remove\add tag budger

--- makeitgreener/templates/test/my.js
+++ makeitgreener/templates/test/my.js
@@ -8,17 +8,25 @@ $(document).keyup(function (e) {
 
     if ($("#select-tag-input:focus") && (e.keyCode === 13)) {
         let selectTagInput = $("#select-tag-input");
-        let newTagName = selectTagInput.val().replace(/ /g,'').toLowerCase();
+        //TODO already in list
+        let newTagName = selectTagInput.val().replace(/ /g, '').toLowerCase();
         if (newTagName !== '') {
             addNewSelectedTagName(newTagName);
             selectTagInput.val('');
         }
-    //    TODO error messge - red
+        //    TODO error messge - red
     }
 });
 
 let addNewSelectedTagName = (tagName) => {
-    $('#selected-tags-list').append(`<h6 class="inline"><span class="badge badge-primary"><a href="#"
-                                                                    style="color: white;text-decoration:none">&times;</a> ${tagName}</span>
+    $('#selected-tags-list').append(`<h6 data-name="${tagName}" class="inline tag-badge">
+               <span class="badge badge-primary"><a href="#" data-name="${tagName}" style="color: white;text-decoration:none">&times;</a> ${tagName}</span>
             </h6>`)
+};
+
+let btnClickRemoveTagFromSelectedTagList = () => {
+    $('div#selected-tags-list').on('click', 'h6 a', function (e) {
+        $(`h6.tag-badge[data-name=${$(this).data("name")}]`).remove();
+        return false;
+    })
 };
